9d0bfc41c1ac3c5fbfe5151d3cd8436739214511
Yashowardhan Pandey <52581482+yashowardhan992@users.noreply.github.com>
2023-09-04 20:51:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-const nodemailer = require("nodemailer");
+const express = require('express');
+const nodemailer = require('nodemailer');
+const path = require('path');
 
-module.exports = async (req, res) => {
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Parse JSON requests
+app.use(express.json());
+
+// Define allowed origins
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+// CORS middleware to allow requests from allowed origins
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // Other CORS headers can be added here if needed
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Define an API endpoint for sending emails
+app.post('/send-email', async (req, res) => {
   try {
     const formData = req.body;
 
@@ -9,7 +35,7 @@ module.exports = async (req, res) => {
 
     // Create a Nodemailer transporter using Gmail SMTP
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      service: 'Gmail',
       auth: {
         user: email,
         pass: password,
@@ -17,7 +43,7 @@ module.exports = async (req, res) => {
       secure: true, // Use TLS
       tls: {
         // For security reasons, only use secure protocols
-        ciphers: "TLS_AES_128_GCM_SHA256",
+        ciphers: 'TLS_AES_128_GCM_SHA256',
       },
     });
 
@@ -25,26 +51,23 @@ module.exports = async (req, res) => {
     const mailOptions = {
       from: email,
       to: formData.email, // Recipient's email from the form
-      subject: "Form Submission",
+      subject: 'Form Submission',
       text: JSON.stringify(formData, null, 2), // Convert the data to a JSON string
     };
-
-
-    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-    }
 
     // Send the email
     await transporter.sendMail(mailOptions);
 
     // Respond with a JSON object containing a 'message' property
-    return res.status(200).json({ message: "Email sent successfully" });
+    return res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error(error);
     // Respond with a JSON object containing an 'error' property
-    return res.status(500).json({ error: "Email could not be sent" });
+    return res.status(500).json({ error: 'Email could not be sent' });
   }
-};
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
